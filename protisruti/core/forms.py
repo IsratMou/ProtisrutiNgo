f9082189636d23f5f36a389bdfb17c62492ded14
IsratMou <moutherobogirl@gmail.com>
2025-05-05 11:00:35 +0600

    Revert "button color change"
    This reverts commit 46b7520d46c08d3a3f567157bc742867ae423839, reversing
    changes made to 5c9d55705d0c38cff9159c4ca794917f3062aeef.

--- protisruti/core/forms.py
+++ protisruti/core/forms.py
@@ -113,15 +113,10 @@ class CustomLoginForm(AuthenticationForm):
 class CounselorAvailabilityForm(forms.Form):
     DAYS_CHOICES = [(i, i) for i in range(1, 32)]  # Days in a month (1-31)
     CATEGORY_CHOICES = [
-<<<<<<< HEAD
         ('domestic_violence', 'Domestic Violence'),
         ('child_abuse', 'Child Abuse'),
         ('sexual_abuse', 'Sexual Abuse'),
         ('trauma', 'Trauma'),
-=======
-        ('heart', 'Heart'),
-        ('mental_health', 'Mental Health'),
->>>>>>> 860289cf76047db18ee215ffa04a0363f729ab43
         ('general', 'General'),
     ]
 
@@ -146,7 +141,6 @@ class IncidentReportForm(forms.Form):
             'rows': 5
         }),
     )
-<<<<<<< HEAD
     schedule_date = forms.DateField(
         label="Schedule Counseling",
         widget=forms.DateInput(attrs={
@@ -154,8 +148,6 @@ class IncidentReportForm(forms.Form):
             'type': 'date'
         }),
     )
-=======
->>>>>>> 860289cf76047db18ee215ffa04a0363f729ab43
 
 class DonationForm(forms.Form):
     donor_name = forms.CharField(
